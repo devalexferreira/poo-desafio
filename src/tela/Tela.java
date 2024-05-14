37b5a2b62dc1d519ec1222b1f66c81cf4be5f18e
afprogram <alex.contato@ymail.com>
2024-05-13 21:40:36 -0300

    Atualizar

--- src/tela/Tela.java
+++ src/tela/Tela.java
@@ -7,7 +7,8 @@ import telefone.Telefone;
 
 public class Tela {
     public static void main(String[] args) throws Exception {
-        
+        Telefone telefone = new Telefone();
+        Equipamento equipamento = telefone;
         Scanner terminal = new Scanner(System.in);
         System.out.println("Escolha uma opção:");
 
